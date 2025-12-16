893a483f468e6b4db5a9982f12d37a23ef182644
DESKTOP-78S4HMK\User <ghost2child@gmail.com>
2025-12-16 09:05:45 +0900

    ItemControlで画像表示、クリックして選択

--- WpfPrismLearn/MainWindowViewModel.cs
+++ WpfPrismLearn/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using WpfPrismLearn.Models;
 using WpfPrismLearn.Services;
 
 namespace WpfPrismLearn
@@ -24,11 +26,36 @@ namespace WpfPrismLearn
         }
 
         public DelegateCommand GreetCommand { get; }
+        public DelegateCommand<ImageItem> SelectImageCommand { get; }
+
+        public ObservableCollection<ImageItem> ImageItems { get; } = new ObservableCollection<ImageItem>();
 
         public MainWindowViewModel(IGreetingService greetingService)
         {
             _greetingService = greetingService;
             GreetCommand = new DelegateCommand(ExecuteGreet);
+
+            SelectImageCommand = new DelegateCommand<ImageItem>(item =>
+            {
+                // 1. 一旦、リスト内のすべての画像の選択を解除する
+                foreach (var image in ImageItems)
+                {
+                    image.IsSelected = false;
+                }
+
+                // 2. クリックされた画像だけを選択状態にする
+                item.IsSelected = true;
+            });
+
+            LoadSampleImages();
+        }
+
+        private void LoadSampleImages()
+        {
+            ImageItems.Add(new ImageItem { Title = "Mountain", ImageUrl = "https://picsum.photos/id/10/200/150" });
+            ImageItems.Add(new ImageItem { Title = "River", ImageUrl = "https://picsum.photos/id/11/200/150" });
+            ImageItems.Add(new ImageItem { Title = "Sea", ImageUrl = "https://picsum.photos/id/12/200/150" });
+            ImageItems.Add(new ImageItem { Title = "Forest", ImageUrl = "https://picsum.photos/id/13/200/150" });
         }
 
         private void ExecuteGreet()
@@ -40,6 +67,12 @@ namespace WpfPrismLearn
             else
             {
                 ResultMessage = _greetingService.GetMessage(InputName);
+
+                ImageItems.Add(new ImageItem
+                {
+                    Title = $"New Item {ImageItems.Count + 1}",
+                    ImageUrl = $"https://picsum.photos/seed/{20 + ImageItems.Count}/200/150"
+                });
             }
         }
     }
---
+++ WpfPrismLearn/Models/ImageItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPrismLearn.Models
+{
+    public class ImageItem : BindableBase
+    {
+        public string Title { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set { SetProperty(ref _isSelected, value); }
+        }
+    }
+}
